e9d91e4c34aae487c23f8d021f6ceeed6656cca0
rxrzzz <temiloluwagithub@gmail.com>
2025-04-02 12:18:19 +0100

    feat: :sparkles: enhance checkbox question options; add icon for adding choices, improve layout and styling, and ensure form state updates correctly

--- src/features/questions-creator/container/checkbox-question-options.tsx
+++ src/features/questions-creator/container/checkbox-question-options.tsx
@@ -3,7 +3,7 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Switch } from "@/components/ui/switch";
-import { Trash2 } from "lucide-react";
+import { Plus, Trash2 } from "lucide-react";
 import {
   Form,
   FormField,
@@ -13,7 +13,6 @@ import {
 } from "@/components/ui/form";
 import { useCheckboxQuestionOptionsForm } from "../form/checboxinput-form";
 import { QuestionOptionsMap } from "@/types/questions";
-
 type LocalQuestionOptions = QuestionOptionsMap["checkbox"];
 
 type OptionProps = {
@@ -30,27 +29,29 @@ export const CheckboxQuestionOptions = memo(
       setQuestionOptions,
     });
 
-    const { control, getValues } = form;
+    const { control, watch, getValues } = form;
+    const choices = watch("choices");
 
     return (
       <Form {...form}>
         <form onSubmit={onSubmit}>
           <div className="flex flex-col gap-4 mb-4">
-            <div>
-              <div className="text-xs mb-4 font-medium justify-between w-full">
-                <p>Choices</p>
+            <div className="border p-4 shadow-xs mt-4">
+              <div className="flex justify-between mb-2">
+                <Label className="text-xs">Choices</Label>
                 <Button
-                  variant="outline"
-                  className="text-xs mt-1"
+                  size={"sm"}
+                  className="text-xs border-none mt-1 w-fit bg-gradient-to-br from-blue-500 to-blue-700 rounded-md overflow-hidden shadow-lg"
                   onClick={() => {
-                    const currentChoices = getValues("choices");
-                    form.setValue("choices", [...currentChoices, ""]);
+                    form.setValue("choices", [...choices, ""], {
+                      shouldDirty: true,
+                    });
                   }}
                 >
-                  Add Choice
+                  <Plus size={19} />
                 </Button>
               </div>
-              <div>
+              <div className="mt-4">
                 {form.getValues("choices").map((_, index) => (
                   <FormField
                     key={index}
@@ -58,7 +59,7 @@ export const CheckboxQuestionOptions = memo(
                     name={`choices.${index}`}
                     render={({ field }) => (
                       <FormItem>
-                        <div className="flex items-center gap-2 mb-2">
+                        <div className="flex items-center gap-2  mb-4">
                           <FormControl>
                             <Input
                               type="text"
@@ -68,6 +69,7 @@ export const CheckboxQuestionOptions = memo(
                           </FormControl>
                           <Button
                             variant="destructive"
+                            className=" max-w-[38px]"
                             onClick={() => {
                               const updatedChoices = getValues(
                                 "choices"
@@ -86,45 +88,43 @@ export const CheckboxQuestionOptions = memo(
               </div>
             </div>
 
-            <div className=" gap-4">
-              <FormField
-                control={control}
-                name="minSelections"
-                render={({ field }) => (
-                  <FormItem>
-                    <Label className="text-xs">Minimum Selections</Label>
-                    <FormControl>
-                      <Input
-                        type="number"
-                        min={0}
-                        {...field}
-                        onChange={(e) => field.onChange(Number(e.target.value))}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+            <FormField
+              control={control}
+              name="minSelections"
+              render={({ field }) => (
+                <FormItem>
+                  <Label className="text-xs">Minimum Selections</Label>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      min={0}
+                      {...field}
+                      onChange={(e) => field.onChange(Number(e.target.value))}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-              <FormField
-                control={control}
-                name="maxSelections"
-                render={({ field }) => (
-                  <FormItem>
-                    <Label className="text-xs">Maximum Selections</Label>
-                    <FormControl>
-                      <Input
-                        type="number"
-                        min={1}
-                        {...field}
-                        onChange={(e) => field.onChange(Number(e.target.value))}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
+            <FormField
+              control={control}
+              name="maxSelections"
+              render={({ field }) => (
+                <FormItem>
+                  <Label className="text-xs">Maximum Selections</Label>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      min={1}
+                      {...field}
+                      onChange={(e) => field.onChange(Number(e.target.value))}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
             <FormField
               control={control}
@@ -141,15 +141,15 @@ export const CheckboxQuestionOptions = memo(
                 </FormItem>
               )}
             />
-          </div>
 
-          <Button
-            type="submit"
-            className="w-full"
-            disabled={!form.formState.isDirty}
-          >
-            Save
-          </Button>
+            <Button
+              type="submit"
+              className="w-full"
+              disabled={!form.formState.isDirty}
+            >
+              Save
+            </Button>
+          </div>
         </form>
       </Form>
     );

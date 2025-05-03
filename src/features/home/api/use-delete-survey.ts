f090be96f59f0d12fab410d61ce46b3443e8c321
Adeleye Temiloluwa <temiloluwagithub@gmail.com>
2025-05-03 17:12:20 +0100

    feat: :sparkles: implement useDeleteSurvey hook for survey deletion functionality

---
+++ src/features/home/api/use-delete-survey.ts
@@ -0,0 +1,39 @@
+import { api } from "@/lib/axios";
+import { useSurveyListStore } from "@/store/surveys.store";
+import { useMutation } from "@tanstack/react-query";
+import { useParams } from "wouter";
+
+type Response = {
+  success: boolean;
+  message: string;
+  data: {
+    id: string;
+  };
+};
+
+export const useDeleteSurvey = () => {
+  const { surveyId } = useParams();
+  const deleteSurveyInStore = useSurveyListStore((state) => state.deleteSurvey);
+
+  const deleteSurveyFunction = async (): Promise<Response> => {
+    const response = await api.delete(`/surveys/${surveyId}`);
+    return response.data;
+  };
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: deleteSurveyFunction,
+    onSuccess: ({data}) => {
+        deleteSurveyInStore(data.id)
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  
+
+  return {
+    deleteSurvey: mutate,
+    loading: isPending,
+  };
+};

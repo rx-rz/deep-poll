a73f68beb6c05838fee075788daa7f1d25306379
Adeleye Temiloluwa <temiloluwagithub@gmail.com>
2025-05-13 11:29:03 +0100

    feat: :sparkles: implement hooks for fetching individual and multiple survey responses; add useGetResponse and useGetSurveyResponses for improved data handling

---
+++ src/features/responses/api/use-get-response.ts
@@ -0,0 +1,46 @@
+import { api } from "@/lib/axios";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "wouter";
+
+type Response = {
+  data: {
+    response: {
+      id: string;
+      surveyId: string;
+      answers: {
+        questionId: string | null;
+        answerText: string | null;
+        answerNumber: number | null;
+        answerJson: any | null;
+        id: string;
+        createdAt: Date;
+        responseId: string | null;
+      }[];
+      submittedAt: Date;
+      account: {
+        user: {
+          email: string | null;
+        };
+      };
+    };
+  };
+};
+export const useGetResponse = () => {
+  const { responseId } = useParams();
+  const getResponse = async (): Promise<Response> => {
+    const response = await api.get(`/responses/${responseId}`);
+    return response.data;
+  };
+
+  const { data, error, isLoading } = useQuery({
+    queryFn: getResponse,
+    queryKey: [`response`, responseId],
+    enabled: !!responseId,
+  });
+
+  return {
+    response: data?.data.response,
+    error,
+    loading: isLoading,
+  };
+};
---
+++ src/features/responses/api/use-get-survey-responses.ts
@@ -0,0 +1,36 @@
+import { api } from "@/lib/axios";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "wouter";
+
+type Response = {
+  success: boolean;
+  message: string;
+  data: {
+    responses: {
+      id: string;
+      surveyId: string | null;
+      accountId: string | null;
+      submittedAt: Date;
+      idempotencyKey: string | null;
+    }[];
+  };
+};
+export const useGetSurveyResponses = () => {
+  const { surveyId } = useParams();
+  const getSurveyResponses = async (): Promise<Response> => {
+    const response = await api.get(`/surveys/${surveyId}/responses`);
+    return response.data;
+  };
+
+  const { data, error, isLoading } = useQuery({
+    queryFn: getSurveyResponses,
+    queryKey: [`survey-responses`, surveyId],
+    enabled: !!surveyId,
+  });
+
+  return {
+    responses: data?.data.responses,
+    error,
+    loading: isLoading
+  };
+};
